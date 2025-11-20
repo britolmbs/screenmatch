6fda8710ef4e14c55f42788832d5572246f6c4d3
Lucas Brito <britolmbs@hotmail.com>
2025-11-20 09:54:54 -0300

    Modelando dados da série

--- src/main/java/br/com/britolmbs/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/britolmbs/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.britolmbs.screenmatch;
 
+import br.com.britolmbs.screenmatch.model.DadosSerie;
 import br.com.britolmbs.screenmatch.service.ConsumoApi;
+import br.com.britolmbs.screenmatch.service.ConverteDado;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +17,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		ConsumoApi consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=Breaking+Bad&Season=1&apikey=599ea6a9");
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=Breaking+Bad&apikey=599ea6a9");
 		System.out.println(json);
+
+		ConverteDado conversor = new ConverteDado();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
