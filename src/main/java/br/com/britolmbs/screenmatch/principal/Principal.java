cc36140b1a3a14f5c7edecf9cdf60856b7e729be
Lucas Brito <britolmbs@hotmail.com>
2025-11-25 10:04:19 -0300

    Nova Classe para lidar com os dados episodios | Buscando episódios a partir de uma data

--- src/main/java/br/com/britolmbs/screenmatch/principal/Principal.java
+++ src/main/java/br/com/britolmbs/screenmatch/principal/Principal.java
@@ -3,9 +3,12 @@ package br.com.britolmbs.screenmatch.principal;
 import br.com.britolmbs.screenmatch.model.DadosEpisodio;
 import br.com.britolmbs.screenmatch.model.DadosSerie;
 import br.com.britolmbs.screenmatch.model.DadosTemporada;
+import br.com.britolmbs.screenmatch.model.Episodio;
 import br.com.britolmbs.screenmatch.service.ConsumoApi;
 import br.com.britolmbs.screenmatch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -61,5 +64,27 @@ public class Principal {
                 .sorted(Comparator.comparing(DadosEpisodio::avalicao).reversed())
                 .limit(5)
                 .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).toList();
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("A partir de qye ano você deseja ver os episodios? ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formator = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+        .forEach(e -> System.out.println(
+                "Temporada: " + e.getTemporada() +
+                        " Episódio: " + e.getTitulo() +
+                        " Data de Lançamento: " + e.getDataLancamento().format(formator)
+        ));
     }
 }
